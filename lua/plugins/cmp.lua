ee38492c8b14466d35f2b09286d1e685cd38b950
keivan-sf <keyvan0082@gmail.com>
2024-05-15 12:40:51 +0330

    Change accept lsp suggestion key map

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -81,7 +81,7 @@ return {
           ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
           ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-          ["<C-y>"] = cmp.config.disable,
+          ["<C-y>"] = cmp.mapping.confirm { select = false },
           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
           ["<CR>"] = cmp.mapping.confirm { select = false },
           ["<Tab>"] = cmp.mapping(function(fallback)
