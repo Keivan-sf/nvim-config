89474806f306dc58de83500cdced41c2e6f977d0
Keivan-sf <keyvan0082@gmail.com>
2025-01-22 16:56:21 +0330

    Change tabsize

--- lua/keive/options.lua
+++ lua/keive/options.lua
@@ -1,10 +1,11 @@
 if vim.fn["has"]("unnamedplus") then
-	vim.opt.clipboard:append({'unnamedplus'})
+	vim.opt.clipboard:append({ "unnamedplus" })
 else
-        vim.opt.clipboard:append({'unnamed'})
+	vim.opt.clipboard:append({ "unnamed" })
 end
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
 vim.g.icons_enabled = true
 vim.g.cmp_enabled = true
