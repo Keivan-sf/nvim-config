bdb4389cf3b54887fdcf6acfc93628e06aa94730
Keivan-sf <keyvan0082@gmail.com>
2025-07-05 11:50:37 +0330

    Change colorscheme

--- lua/keive/after/init.lua
+++ lua/keive/after/init.lua
@@ -1,4 +1,6 @@
-vim.cmd.colorscheme("github_dark_default")
+-- color theme
+-- vim.cmd.colorscheme("github_dark_default")
+vim.cmd.colorscheme("murphy")
 vim.cmd("TSEnable 1")
 require("keive.after.lsp")
 require("keive.after.cmp")
